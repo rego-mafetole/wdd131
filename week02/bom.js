d72302331f22256355d8ad67870121a624c8b789
rego-mafetole <rmafetole@gmail.com>
2026-03-14 20:08:47 +0100

    Temple Album

--- week02/bom.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('_____');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
